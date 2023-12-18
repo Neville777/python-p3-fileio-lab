cb4b2293525d255c2d812b939ee5fd6bd65f460a
neville777 <neville.james@student.moringaschool.com>
2023-12-18 07:29:37 +0300

    fileio-lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
+# lib/file_io.py
+
 def write_file(file_name, file_content):
-    pass
+    file_name_with_extension = str(file_name) + ".txt"
+    with open(file_name_with_extension, "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_name_with_extension = str(file_name) + ".txt"
+    with open(file_name_with_extension, "a") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_name_with_extension = str(file_name) + ".txt"
+    with open(file_name_with_extension, "r") as file:
+        content = file.read()
+    return content
+
+        
